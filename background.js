3e3a34bb5e7055dc8c7e1ac78d2dc858634a045e
Vasilj Milošević <eboyee@gmail.com>
2022-12-17 05:59:44 +0100

    manifest v3 support

--- background.js
+++ background.js
@@ -1,10 +1,14 @@
 /*global browser, console*/
-function onTabLoad(details) {
-  'use strict';
-  browser.tabs.executeScript(details.tabId, {
-    file: 'srbtranslit.js',
-    allFrames: true
-  });
-}
-
-browser.browserAction.onClicked.addListener(onTabLoad);
+browser.action.onClicked.addListener(async (tab) => {
+  try {
+    await browser.scripting.executeScript({
+      target: {
+        tabId: tab.id,
+        allFrames: true,
+      },
+      files: ['srbtranslit.js'],
+    });
+  } catch (err) {
+    console.error(`failed to execute script: ${err}`);
+  }
+});
